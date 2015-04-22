4d26d1f03cd5ff95ab77e5b711ee247683002e04
khrall <raymitoro@gmail.com>
2015-04-22 15:05:22 +0100

    Attempt 1

---
+++ snes.v1/src/snes/v1/pintest/component/Component.java
@@ -0,0 +1,11 @@
+package snes.v1.pintest.component;
+
+import no.ntnu.item.arctis.runtime.Block;
+
+public class Component extends Block {
+
+	public void testOutput() {
+		System.out.println("Hello Pi:T6");
+	}
+
+}
